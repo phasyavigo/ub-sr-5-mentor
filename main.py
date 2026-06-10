3967ea3db9d6c59fc13d910c647b63e3702935be
phasyavigo <phasyavigo@gmail.com>
2026-06-10 12:55:33 +0700

    kosongan

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI,
+from routers import mentor_chat, mentor_essay_evaluasi, mentor_pilgan_evaluasi
+
+app = FastAPI()
+
+app.include_router(mentor_chat.router)
+app.include_router(mentor_pilgan_evaluasi.router)
+app.include_router(mentor_essay_evaluasi.router)
+
+@app.get("/")
+def root():
+    return {"message": "Backend Tim 5"}
